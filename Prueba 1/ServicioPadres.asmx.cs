fbd300c382564f0734edc8ff3cbc3684aeef3b32
SebastiaN <odm_sd@hotmail.com>
2018-11-26 09:57:03 -0500

    prueba 1

---
+++ Prueba 1/ServicioPadres.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Logica;
+using AccesoADatos;
+
+namespace ServiciosWeb
+{
+    /// <summary>
+    /// Descripción breve de ServicioPadres
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicioPadres : System.Web.Services.WebService
+    {
+
+        Logica_Padre logica = new Logica_Padre();
+
+        [WebMethod]
+        public List<personas> SeleccionarPadres() => logica.SeleccionarPersonas();
+    }
+}
