c6f7edb1fce1beb4d4e61194a4b8684cc1c1b713
SebastiaN <odm_sd@hotmail.com>
2018-12-12 10:35:54 -0500

    wcf

---
+++ WCF/App_Code/IServicio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AccesoADatos;
+using Logica;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicio" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IServicio
+{
+    [OperationContract]
+    List<computadoras> Seleccionar();
+}
---
+++ WCF/App_Code/Servicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AccesoADatos;
+using Logica;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "Servicio" en el código, en svc y en el archivo de configuración a la vez.
+public class Servicio : IServicio
+{
+    CompuLogica logica = new CompuLogica();
+    public List<computadoras> Seleccionar()
+    {
+        return logica.SeleccionarCompusMantenimiento();
+    }
+}
