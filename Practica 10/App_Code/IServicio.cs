2573b75ac85fce745d2de3e177403777a656e54a
SebastiaN <odm_sd@hotmail.com>
2018-12-16 12:54:53 -0500

    practica 10 backend

--- Practica 10/App_Code/IServicio.cs
+++ Practica 10/App_Code/IServicio.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using AccesoADatos;
-using Logica;
+using Acceso;
 
 // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicio" en el código y en el archivo de configuración a la vez.
 [ServiceContract]
@@ -13,4 +12,14 @@ public interface IServicio
 {
     [OperationContract]
     List<computadoras> Seleccionar();
+
+    [OperationContract]
+    bool Actualizar(computadoras datos);
+
+    [OperationContract]
+    bool Eliminar(computadoras datos);
+
+    [OperationContract]
+    computadoras Insertar(computadoras datos);
+
 }
--- Practica 10/App_Code/Servicio.cs
+++ Practica 10/App_Code/Servicio.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using AccesoADatos;
+using Acceso;
 using Logica;
 
 // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "Servicio" en el código, en svc y en el archivo de configuración a la vez.
 public class Servicio : IServicio
 {
-    CompuLogica logica = new CompuLogica();
+    Parseo logica = new Parseo();
     public List<computadoras> Seleccionar()
     {
-        return logica.SeleccionarCompusMantenimiento();
+        return logica.SeleccionarTodas();
+    }
+
+    public bool Actualizar(computadoras datos)
+    {
+        return logica.ActualizarComputador(datos);
+    }
+
+    public bool Eliminar(computadoras datos)
+    {
+        return logica.EliminarCompu(datos);
+    }
+
+    public computadoras Insertar(computadoras datos)
+    {
+        return logica.InsertarComputador(datos);
     }
 }
