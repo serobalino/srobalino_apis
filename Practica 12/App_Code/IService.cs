531b2da9924cb8adec2ad4e61b819bd74f9d8682
SebastiaN <odm_sd@hotmail.com>
2018-12-17 08:54:58 -0500

    practica 12

---
+++ Practica 12/App_Code/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Aceso;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IService" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    List<personas> Seleccionar();
+
+    [OperationContract]
+    bool Actualizar(personas datos);
+
+    [OperationContract]
+    bool Eliminar(personas datos);
+
+    [OperationContract]
+    personas Insertar(personas datos);
+}
---
+++ Practica 12/App_Code/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Aceso;
+using Logica;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "Service" en el código, en svc y en el archivo de configuración a la vez.
+public class Service : IService
+{
+    Serializador logica = new Serializador();
+    public List<personas> Seleccionar()
+    {
+        return logica.SeleccionarTodas();
+    }
+
+    public bool Actualizar(personas datos)
+    {
+        return logica.ActualizarPersona(datos);
+    }
+
+    public bool Eliminar(personas datos)
+    {
+        return logica.EliminarPersona(datos);
+    }
+
+    public personas Insertar(personas datos)
+    {
+        return logica.InsertarPersona(datos);
+    }
+}
